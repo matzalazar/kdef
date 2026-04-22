f4ab5871ff375b56adf2571547a9f965b1ff4fd3
Matías Zalazar <matias.zalazar@icloud.com>
2026-04-22 18:46:42 -0300

    feat: añado despliegue automático sin scraping en cada push

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -9,8 +9,8 @@ export default ((_opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>cultivado con ❤️ por <a href="https://github.com/matzalazar">matzalazar</a></p>
-        <p dangerouslySetInnerHTML={{__html: '¿querés replicar esto? <!-- email_off --><a href="mailto:matias.zalazar@icloud.com">escribime</a><!-- /email_off -->'}}></p>
+        <p>cultivado con ❤️ por <a href="https://matzalazar.com">matzalazar</a></p>
+        <p dangerouslySetInnerHTML={{__html: '¿querés replicar esto? <!-- email_off --><a href="mailto:contacto@matzalazar.com">escribime</a><!-- /email_off -->'}}></p>
       </footer>
     )
   }
